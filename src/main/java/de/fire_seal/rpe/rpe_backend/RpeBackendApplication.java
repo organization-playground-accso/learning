09ee1261e68ca3f46c6e7468040c4bd3315561fd
Lea Fries <lea.fries@accso.de>
2024-11-11 16:52:33 +0100

    :zap: init skeleton

---
+++ src/main/java/de/fire_seal/rpe/rpe_backend/RpeBackendApplication.java
@@ -0,0 +1,13 @@
+package de.fire_seal.rpe.rpe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fire_seal/rpe/rpe_backend/RpeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fire_seal.rpe.rpe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
